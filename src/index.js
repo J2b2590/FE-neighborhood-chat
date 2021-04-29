9b719edd7c7a1e6444188c67d2059da18545fd45
J2b2590 <J2b2590@gmail.com>
2021-04-29 10:52:18 -0500

    action cable url

--- src/index.js
+++ src/index.js
@@ -1,37 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import 'semantic-ui-css/semantic.min.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "semantic-ui-css/semantic.min.css";
 
-
-import { Provider } from 'react-redux'
-import { createStore } from 'redux';
-import { BrowserRouter } from 'react-router-dom'
-import rootReducer from './reducers';
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import { BrowserRouter } from "react-router-dom";
+import rootReducer from "./reducers";
 // import createConsumer from '@rails/actioncable'
-import actionCable from 'actioncable'
+import actionCable from "actioncable";
 
+import API from "./Api";
 
 let store = createStore(
   rootReducer,
-  {user: {username:""}},
+  { user: { username: "" } },
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-)
+);
 
-const CableApp = {}
+const CableApp = {};
 
-CableApp.cable = actionCable.createConsumer('ws://localhost:3000/cable') 
+CableApp.cable = actionCable.createConsumer(`ws://${API}/cable`);
 
 // console.log({CableApp})
 ReactDOM.render(
   <Provider store={store}>
-        <BrowserRouter>
-          <App cableApp={CableApp} />
-        </BrowserRouter>
+    <BrowserRouter>
+      <App cableApp={CableApp} />
+    </BrowserRouter>
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
