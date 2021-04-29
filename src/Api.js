229ef94a3fa5c51ee10c93711c083e86ff826e16
J2b2590 <J2b2590@gmail.com>
2021-04-29 13:14:20 -0500

    revert to 7aaf2ad

--- src/Api.js
+++ src/Api.js
@@ -1,5 +1,3 @@
 const API = `https://neighbor-chat-be.herokuapp.com`;
 // const API = `http://localhost:3000`;
-// const wsAPI = "ws://localhost:3000/cable";
-
 export default API;
--- src/index.js
+++ src/index.js
@@ -12,6 +12,8 @@ import rootReducer from "./reducers";
 // import createConsumer from '@rails/actioncable'
 import actionCable from "actioncable";
 
+import API from "./Api";
+
 let store = createStore(
   rootReducer,
   { user: { username: "" } },
@@ -20,11 +22,9 @@ let store = createStore(
 
 const CableApp = {};
 
-CableApp.cable = actionCable.createConsumer(
-  "http://neighbor-chat-be.herokuapp.com/cable"
-);
+CableApp.cable = actionCable.createConsumer(`ws://${API}/cable`);
 
-console.log({ CableApp });
+// console.log({CableApp})
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
